274b04a6aeebbf3a0738d59274de9810f4496553
Rutujshahare <shaharerutuj23@gmail.com>
2026-02-12 19:09:22 +0530

    added the maven producer project

---
+++ OlaCabDriver/src/main/java/com/example/OlaCabDriver/OlaCabDriverApplication.java
@@ -0,0 +1,13 @@
+package com.example.OlaCabDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaCabDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaCabDriverApplication.class, args);
+	}
+
+}
---
+++ OlaCabDriver/src/test/java/com/example/OlaCabDriver/OlaCabDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OlaCabDriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaCabDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
