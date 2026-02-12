0f148121f05960ae075b2cb76e908b9459591fed
Rutujshahare <shaharerutuj23@gmail.com>
2026-02-12 20:30:42 +0530

    added a consumer project

---
+++ BookPerson/src/test/java/com/example/BookPerson/BookPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookPerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
